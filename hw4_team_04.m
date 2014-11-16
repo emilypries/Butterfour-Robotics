4458866089408d2c6a44ca5bb51971c0472d056e
Ernesto Sandoval <escrnesto@gmail.com>
2014-11-15 23:47:16 -0500

    started hw4

---
+++ hw4_team_04.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COMS W4733 Computational Aspects of Robotics Fall 2014
+%
+% Homework 4
+%
+% Team Number:  4
+% Team Leader:  Luis Tolosa (let2120)
+% Team Members: Emily Pries (ep2642)
+%               Ernesto Sandoval Castillo (es3187) 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+% main function - 
+% port is robot object returned by RoombaInit
+function hw4_team_04(port)
