97ae38ebc2eb739ec3ae7cc004e7b68340bee24f
Ernesto Sandoval <escrnesto@gmail.com>
2014-12-07 01:13:17 -0500

    first step

--- hw5_team_04_1.m
+++ hw5_team_04_1.m
@@ -1,9 +1,35 @@
-function CameraTest()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COMS W4733 Computational Aspects of Robotics Fall 2014
+%
+% Homework 5
+%
+% Team Number:  4
+% Team Leader:  Luis Tolosa (let2120)
+% Team Members: Emily Pries (ep2642)
+%               Ernesto Sandoval Castillo (es3187) 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Task #1 - Color Marker Tracking
+% 'port' is robot object returned by RoombaInit()
+
+function hw5_team_04_1(port)
+    err = 10;
+    % spatial descriptors of robot
+    xpos = 0;
+    ypos = 0;
+    ang  = 0;
+    % states of robot during process
+    FIND_OBJ = 0;
+    TURN2OBJ = 1;
+    MOVE_FWD = 2;
+    cur_state = FIND_OBJ;
     
-    %Will get an initial snapshot of the camera and will get a single input
-    %from the user which will decide the target color
+    % will get an initial snapshot of the camera and a single
+    % input from the user which will decide the target color
     str_ip = 'http://192.168.0.100/snapshot.cgi?user=admin&pwd=&resolution=16&rate=0';
     img = imread(str_ip);
+    axis = size(img,2)/2;
     fh = figure; imshow(img);
     h = imrect;
     rect = round(h.getPosition);
@@ -12,26 +38,56 @@ function CameraTest()
     %Extract values from rect
     width = rect(3);
     height = rect(4);
-    x = rect(1) + round(width/2);
-    y = rect(2) + round(height/2);
 
     %Gets target color, CURRENTLY RGB 
     img_hsv = rgb2hsv(img);
-    trgt_window = img_hsv(rect(2):rect(2)+height,rect(1):rect(1)+width,:)
-    trgt = mean(mean(trgt_window))
+    trgt_window = img_hsv(rect(2):rect(2)+height,rect(1):rect(1)+width,:);
+    trgt = mean(mean(trgt_window));
     hr = 0.03;
     sr = 0.16;
     vr = 0.07;
     
     fh = figure;
     
-    %Endless loop to test camera, quit with ctrl c :)
+    [xc, yc, ta] = hw5_track(img,trgt,hr,sr,vr);
+    
     while true
-        
+        img = imread(str_ip);
+        [x,y,a] = hw5_track(img,trgt,hr,sr,vr);
+        [xpos,ypos,ang] = adjust_dist(port,xpos,ypos,ang);
+        scale = abs(x - axis)/axis;
+        if cur_state == FIND_OBJ
+            if x < axis
+                SetDriveWheelsCreate(port,0.2*scale,-0.2*scale);
+            elseif x > axis
+                SetDriveWheelsCreate(port,-0.2*scale,0.2*scale);
+            end
+            cur_state = TURN2OBJ;
+        elseif cur_state == TURN2OBJ
+            SetDriveWheelsCreate(port,0.0,0.0);
+            if a < ta
+                SetDriveWheelsCreate(port,0.1,0.1);
+                cur_state = MOVE_FWD;
+            elseif a > ta
+                SetDriveWheelsCreate(port,-0.1,-0.1);
+                cur_state = MOVE_FWD;
+            else
+                cur_state = FIND_OBJ;
+            end
+        elseif cur_state == MOVE_FWD
+            if a > (ta - err) && a < (ta + err)
+                SetDriveWheelsCreate(port,0.0,0.0);
+                cur_state = FIND_OBJ;
+            end
+        end
+        pause(0.05);    
+    end
+end
+
+function [x_cen,y_cen,A] = hw5_track(img_rgb,trgt,hr,sr,vr)   
         %Reads current image
-        img = rgb2hsv(imread(str_ip));
-        imwrite(img, 'hsv.png');
-        imwrite(hsv2rgb(img), 'origs.png');
+        img = rgb2hsv(img_rgb);
+        
         %Sets up and constructs mask
         h_mask_L = img(:, :, 1) > (trgt(1) - hr);
         h_mask_H = img(:, :, 1) < (trgt(1) + hr);
@@ -47,13 +103,13 @@ function CameraTest()
         %Performs 15 dilations and erosions. Fills in holes in blobs that
         %are meant to connect together
         k = 10;
-        processed_img = bwmorph(avg_mask, 'erode', k/2);
+        processed_img = bwmorph(avg_mask, 'erode', k);
         processed_img = bwmorph(processed_img, 'dilate', k);
 
         %Performs 15 erosions and 15 dilations. Removes some random noise in
         %the image
-        processed_img = bwmorph(processed_img, 'erode', k/2);
         processed_img = bwmorph(processed_img, 'dilate', k);
+        processed_img = bwmorph(processed_img, 'erode', k);
         
         in_mask = processed_img;
         
@@ -82,8 +138,7 @@ function CameraTest()
         A = sum(sum(mask));
         x = 0;
         y = 0;
-        [m_mask, n_mask] = size(mask);
-        
+        [m_mask, n_mask] = size(mask);        
         for i = 1:m_mask
             for j = 1:n_mask
                 if(mask(i, j))
@@ -110,6 +165,18 @@ function CameraTest()
             'r-', 'MarkerFaceColor', [1 0 0]);
         pause(0.05);
         hold off;
-    end
+end
 
+% updates the spatial descriptors
+function [x, y, r] = adjust_dist(port, a, b, rad) 
+    r = rad + AngleSensorRoomba(port);
+    if (r < 0)
+        r = r + (2*pi);
+    end
+    if (r > 2*pi)
+        r = r - (2*pi);
+    end
+    d = DistanceSensorRoomba(port)/.35;
+    x = a+d*cos(r); % values will be displayed
+    y = b+d*sin(r);
 end
---
+++ hw5_team_04_2.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% COMS W4733 Computational Aspects of Robotics Fall 2014
+%
+% Homework 5
+%
+% Team Number:  4
+% Team Leader:  Luis Tolosa (let2120)
+% Team Members: Emily Pries (ep2642)
+%               Ernesto Sandoval Castillo (es3187) 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Task #2 - Find, Knock, & Enter CEPSR 6 Door
+function hw5_team_04_2(port)
+end
