4c967a225ab890d57acf0493469691946a3060dc
LTolosa <let2120@columbia.edu>
2014-12-08 11:30:53 -0500

    more changes to CameraTest
    h & s work better than h&s&v? Range testing!

--- CameraTest.m
+++ CameraTest.m
@@ -15,13 +15,13 @@ function CameraTest()
     x = rect(1) + round(width/2);
     y = rect(2) + round(height/2);
 
-    %Gets target color, CURRENTLY RGB 
+    %Gets target color, CURRENTLY HSV
     img_hsv = rgb2hsv(img);
-    trgt_window = img_hsv(rect(2):rect(2)+height,rect(1):rect(1)+width,:)
+    trgt_window = img_hsv(rect(2):rect(2)+height,rect(1):rect(1)+width,:);
     trgt = mean(mean(trgt_window))
-    hr = 0.03;
+    hr = 0.05;
     sr = 0.16;
-    vr = 0.07;
+    vr = 0.15;
     
     fh = figure;
     
@@ -31,7 +31,11 @@ function CameraTest()
         %Reads current image
         img = rgb2hsv(imread(str_ip));
         imwrite(img, 'hsv.png');
-        imwrite(hsv2rgb(img), 'origs.png');
+        rgb = hsv2rgb(img);
+        subplot(2, 3, 4);
+        imshow(rgb); title('RGB');
+        subplot(2, 3, 6);
+        imshow(img); title('HSV');
         %Sets up and constructs mask
         h_mask_L = img(:, :, 1) > (trgt(1) - hr);
         h_mask_H = img(:, :, 1) < (trgt(1) + hr);
@@ -40,19 +44,25 @@ function CameraTest()
         v_mask_L = img(:, :, 3) > (trgt(3) - vr);
         v_mask_H = img(:, :, 3) < (trgt(3) + vr);
         avg_mask = (h_mask_L == h_mask_H) &...
-                   (s_mask_L == s_mask_H) &...
-                   (v_mask_L == v_mask_H);
-                
+                   (s_mask_L == s_mask_H);% &...
+                   %(v_mask_L == v_mask_H);
+        subplot(2, 3, 1);
+        imshow((h_mask_L == h_mask_H)); title('Hue');
+        subplot(2, 3, 2);
+        imshow((s_mask_L == s_mask_H)); title('Saturation');
+        subplot(2, 3, 3);
+        imshow((v_mask_L == v_mask_H)); title('Value');
+        
     
         %Performs 15 dilations and erosions. Fills in holes in blobs that
         %are meant to connect together
         k = 10;
-        processed_img = bwmorph(avg_mask, 'erode', k/2);
-        processed_img = bwmorph(processed_img, 'dilate', k);
+        processed_img = bwmorph(avg_mask, 'dilate', k);
+        processed_img = bwmorph(processed_img, 'erode', k);
 
         %Performs 15 erosions and 15 dilations. Removes some random noise in
         %the image
-        processed_img = bwmorph(processed_img, 'erode', k/2);
+        processed_img = bwmorph(processed_img, 'erode', k);
         processed_img = bwmorph(processed_img, 'dilate', k);
         
         in_mask = processed_img;
@@ -97,7 +107,8 @@ function CameraTest()
         x_cen = floor(x/A);
         y_cen = floor(y/A);
 
-        imshow(mask);
+        subplot(2,3,5);
+        imshow(mask); title('Mask');
         hold on;
         %Draws square over centroid.
         plot([x_cen-10;x_cen+10], [y_cen+10;y_cen+10],...
@@ -110,6 +121,7 @@ function CameraTest()
             'r-', 'MarkerFaceColor', [1 0 0]);
         pause(0.05);
         hold off;
+        
     end
 
 end
